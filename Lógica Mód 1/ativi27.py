5a9a5a81bbef241b54dc05690fa04e7ece0d0b39
Emanuela Alves <111780204+EmanuelaAlves@users.noreply.github.com>
2023-03-18 15:25:05 -0300

    Add files via upload

---
+++ Lógica Mód 1/ativi27.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        num = int(input("Digite um número inteiro: "))
+        numero = int
+        print(" È um número.")
+        break
+    except ValueError:
+        print("Valor inválido. Digite um número inteiro válido.")
