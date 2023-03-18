a1f9f49e6f078ee2e4f32b459ec271b404047eda
Emanuela Alves <111780204+EmanuelaAlves@users.noreply.github.com>
2023-03-18 11:47:51 -0300

    Add files via upload

---
+++ Lógica Mód 1/ativi25.py
@@ -0,0 +1,11 @@
+#método capitalize 
+nome = "bruno"
+print(nome.capitalize())
+
+#método Cout
+cor = "vermelho"
+print(cor.count("e"))
+
+#método Strip
+frase = ("Atividade Lógica de Programação")
+print("Atividade" + "Lógica" + "de" + "Programação")
